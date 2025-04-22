02c576581295a4cc2a9ec1bbb3d331b6255200fd
ljhee92 <weilainib7@gmail.com>
2025-04-22 16:29:27 +0900

    feat: 학습테스트 - Spring Bean

--- spring-core-1/initial/src/main/java/cholog/di/ConstructorInjection.java
+++ spring-core-1/initial/src/main/java/cholog/di/ConstructorInjection.java
@@ -7,8 +7,12 @@ public class ConstructorInjection {
     private InjectionBean injectionBean;
 
     /*
-    ConstructorInjection으로 InjectionBean 주입받기
-     */
+        ConstructorInjection으로 InjectionBean 주입받기
+         */
+    public ConstructorInjection(InjectionBean injectionBean) {
+        this.injectionBean = injectionBean;
+    }
+
     public String sayHello() {
         return injectionBean.hello();
     }
