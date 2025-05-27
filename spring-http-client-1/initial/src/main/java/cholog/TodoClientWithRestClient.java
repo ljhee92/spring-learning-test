5fa9534dea61f296d9a0f7caf02109381ee1b8e9
ljhee92 <weilainib7@gmail.com>
2025-05-27 14:19:15 +0900

    feat: 학습테스트 - HTTP Client

--- spring-http-client-1/initial/src/main/java/cholog/TodoClientWithRestClient.java
+++ spring-http-client-1/initial/src/main/java/cholog/TodoClientWithRestClient.java
@@ -1,11 +1,16 @@
 package cholog;
 
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestClient;
 
 import java.util.Collections;
 import java.util.List;
 
 public class TodoClientWithRestClient {
+
+    private static final String URI = "/todos";
+
     private final RestClient restClient;
 
     public TodoClientWithRestClient(RestClient restClient) {
@@ -14,12 +19,23 @@ public class TodoClientWithRestClient {
 
     public List<Todo> getTodos() {
         // TODO: restClient의 get 메서드를 사용하여 요청을 보내고 결과를 Todo 리스트로 변환하여 반환
-        return Collections.emptyList();
+        return restClient.get()
+                .uri(URI)
+                .retrieve()
+                .body(new ParameterizedTypeReference<>() {
+                });
     }
 
     public Todo getTodoById(Long id) {
         // TODO: restClient의 get 메서드를 사용하여 요청을 보내고 결과를 Todo로 변환하여 반환
         // TODO: 존재하지 않는 id로 요청을 보낼 경우 TodoException.NotFound 예외를 던짐
-        return new Todo();
+        return restClient.get()
+                .uri(URI + "/" + id)
+                .retrieve()
+                .onStatus(status -> status.value() == 404, (req, res) -> {
+                    throw new TodoException.NotFound(id);
+                })
+                .body(Todo.class);
+
     }
 }
--- spring-http-client-1/initial/src/main/java/cholog/TodoClientWithRestTemplate.java
+++ spring-http-client-1/initial/src/main/java/cholog/TodoClientWithRestTemplate.java
@@ -1,8 +1,13 @@
 package cholog;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 public class TodoClientWithRestTemplate {
+
+    private static final String BASE_URL = "http://jsonplaceholder.typicode.com/todos/";
+
     private final RestTemplate restTemplate;
 
     public TodoClientWithRestTemplate(RestTemplate restTemplate) {
@@ -11,7 +16,13 @@ public class TodoClientWithRestTemplate {
 
     public Todo getTodoById(Long id) {
         // TODO: restTemplate을 사용하여 요청을 보내고 결과를 Todo로 변환하여 반환
-        // TODO: 존재하지 않는 id로 요청을 보낼 경우 TodoException.NotFound 예외를 던짐
-        return new Todo();
+        try {
+            ResponseEntity<Todo> response = restTemplate.getForEntity(BASE_URL + id, Todo.class);
+
+            return response.getBody();
+        } catch (HttpClientErrorException e) {
+            // TODO: 존재하지 않는 id로 요청을 보낼 경우 TodoException.NotFound 예외를 던짐
+            throw new TodoException.NotFound(id);
+        }
     }
 }
